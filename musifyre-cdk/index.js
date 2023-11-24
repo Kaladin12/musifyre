2971836852fdc564f20cd4b5252ecc9d1409a4a9
Kaladin12 <eliancruz998@gmail.com>
2023-11-23 21:22:11 -0800

    adding declarations

--- musifyre-cdk/index.js
+++ musifyre-cdk/index.js
@@ -1,2 +1,6 @@
-import { MusifyreCdkStack } from './lib/musifyre-cdk-stack';  
-export { MusifyreCdkStack };
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MusifyreCdkStack = void 0;
+const musifyre_cdk_stack_1 = require("./lib/musifyre-cdk-stack");
+Object.defineProperty(exports, "MusifyreCdkStack", { enumerable: true, get: function () { return musifyre_cdk_stack_1.MusifyreCdkStack; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxpRUFBNEQ7QUFDbkQsaUdBREEscUNBQWdCLE9BQ0EiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBNdXNpZnlyZUNka1N0YWNrIH0gZnJvbSAnLi9saWIvbXVzaWZ5cmUtY2RrLXN0YWNrJzsgIFxuZXhwb3J0IHsgTXVzaWZ5cmVDZGtTdGFjayB9O1xuIl19
